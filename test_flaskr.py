471d70e24bffef6b6e53b78706f7f6fa6008f2c3
Yaser Eisa <yasereisa@Yasers-MacBook-Air.local>
2021-09-11 22:42:02 -0400

    first commit

---
+++ test_flaskr.py
@@ -0,0 +1,94 @@
+import os
+import unittest
+import json
+
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from models import setup_db, Question, Category, db
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        question = {
+        "question": "new Test question?",
+        "answer":"Test answer",
+        "difficulty":1,
+        "category":1
+
+        }
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+    def tearDown(self):
+        pass
+
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['categories'])
+
+    def test_add_question(self):
+        res =self.client().post('/questions',
+        json= {"question": "Test question?", "answer":"Test answer", "difficulty":1, "category":1 })
+        data=json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'],True)
+
+    def test_get_questions_by_Category(self):
+        res = self.client().get('/categories/5/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['totalQuestions'])
+        self.assertTrue(data['questions'])
+
+    def test_get_false_q_by_Category(self):
+        res = self.client().get('/categories/100/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'],False)
+
+    def test_Categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['categories'])
+
+
+
+
+
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
